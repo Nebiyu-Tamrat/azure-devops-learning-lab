0233950c648b930c3e169ee63bf3ec3ecc86bf80
Nebiyu Tamrat <278914918+Nebiyu-Tamrat@users.noreply.github.com>
2026-08-24 21:15:03 +0200

    Added linux health check disk utilization

--- scripts/linux-health-check.sh
+++ scripts/linux-health-check.sh
@@ -26,3 +26,21 @@ else
     echo "Memory Status: OK"
 fi
 
+DISK_THRESHOLD=80
+
+echo ""
+echo "Disk Usage:"
+
+df -h --output=target,pcent | awk 'NR>1 {
+    gsub("%","",$2)
+    printf "%-20s %s%%\n", $1, $2
+}'
+
+ROOT_DISK_USAGE=$(df / | awk 'NR==2 {gsub("%","",$5); print $5}')
+
+if [ "$ROOT_DISK_USAGE" -gt "$DISK_THRESHOLD" ]; then
+    echo "Disk Status: WARNING"
+else
+    echo "Disk Status: OK"
+fi
+
